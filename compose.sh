49d58568218968839f6a2c2828aa1b7b17c44ba8
Eric Fossas <eric.fossas@EFOSSAS-M2233.local>
2018-08-20 14:24:45 -0600

    node_modules

--- compose.sh
+++ compose.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# install node_modules on host
+docker run -itd -v ./:/poc --name tmp_wsapoc
+sleep 5
+docker exec -it -w /poc tmp_wsapoc npm install
+docker stop tmp_wsapoc && docker rm tmp_wsapoc
+
+# 
 docker build --no-cache -f Dockerfile -t efossas/wsapoc .
-docker-compose -p wsapoc up -d
+docker-compose up -d
