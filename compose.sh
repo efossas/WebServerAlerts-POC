9966911c5f3abf0b82447146ef99d25805e36849
Eric Fossas <eric.fossas@EFOSSAS-M2233.local>
2018-08-20 14:28:52 -0600

    node_modules

--- compose.sh
+++ compose.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install node_modules on host
-docker run -itd -v ./:/poc --name tmp_wsapoc
+docker run -itd -v ${PWD}/:/poc --name tmp_wsapoc node
 sleep 5
 docker exec -it -w /poc tmp_wsapoc npm install
 docker stop tmp_wsapoc && docker rm tmp_wsapoc
